1eb45daf9ec74534b7b4d8e8dfd349bd00b8d994
developer <agraharirehan57@gmail.com>
2025-04-21 18:09:02 +0545

    new

---
+++ script.js
@@ -0,0 +1,42 @@
+const btn = document.querySelector(".button");
+const errorMessage = document.querySelector(".error");
+const containers = document.querySelector(".container");
+
+btn.addEventListener("click", () => {
+  const name = document.querySelector("#name").value;
+  const spouseName = document.querySelector("#spouse").value;
+
+  if (name === "ram" && spouseName === "ram") {
+    name.value = "";
+    spouseName.value = "";
+    errorMessage.innerHTML = "";
+    containers.style.display = "none";
+    document.querySelector(".paper").style.display = "block";
+    localStorage.setItem("name", name);
+    localStorage.setItem("spouse", spouseName);
+  } else {
+    errorMessage.innerHTML = "worng please try again";
+    name.value = "";
+    spouseName.value = "";
+  }
+});
+
+window.addEventListener("load", function () {
+  const storedName = localStorage.getItem("name");
+  const storedSpouse = localStorage.getItem("spouse");
+
+  const name = document.querySelector("#name");
+  const spouseName = document.querySelector("#spouse");
+
+  if (name) {
+    name.value = storedName;
+  }
+  if (spouseName) {
+    spouseName.value = storedName;
+  }
+
+  if (storedName && storedSpouse) {
+    containers.style.display = "none";
+    document.querySelector(".paper").style.display = "block";
+  }
+});
